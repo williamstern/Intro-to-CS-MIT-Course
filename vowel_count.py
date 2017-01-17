ffe131fa197cb5e1a5593e8e90088db798fa2aae
williamstern <wmstern@stu.naperville203.org>
2017-01-17 11:07:04 -0800

    Add files via upload

---
+++ vowel_count.py
@@ -0,0 +1,9 @@
+t_l = "alflkdsghsldghlksdfjaikldsg" #total letters
+counter = 0
+times_run = 0
+for letters in(t_l):
+		for letters in (str('a'), str('e'), str('i'), str('o'), str('u')):
+			counter += 1
+	#counter += 1
+print('Number of vowels: ', counter)
+	
