48446c1ee33fb09d999b45b8f427bc0935469553
williamstern <wmstern@stu.naperville203.org>
2017-02-02 16:15:18 -0600

    Update Set 2 Problem 3.py

--- Set 2 Problem 3.py
+++ Set 2 Problem 3.py
@@ -1,20 +1,28 @@
 month = 0 
-balance = 4773
+#balance = 4773
 bottom = balance * (1/12)
-annualInterestRate = 0.2
-balancestat = balance
-payment = 0
+top = balance
+midpoint = 0
 monthlyinterestrate = annualInterestRate / 12
+#annualInterestRate = 0.2
+balancestat = (balance * (1 +  monthlyinterestrate)**12) / 12
+payment = 0
 while month < 12:
-   month += 1
-   unpaidbalance = balance - payment
-   interest = unpaidbalance * monthlyinterestrate
-   balance = interest + unpaidbalance
-   payment * month
-   if month == 12 and balance > 0:
-	   payment += 10
-	   month = 0
-	   balance = balancestat
-if balance <= 0:
-  print("Lowest Payment:", payment)
-  #print(balance)
+    midpoint = (top - bottom) /2
+    month += 1
+    unpaidbalance = balance - payment
+    interest = unpaidbalance * monthlyinterestrate
+    balance = interest + unpaidbalance
+    midpoint * month
+    if month == 12 and balance > -0.01:
+        top = midpoint
+        midpoint = (top - bottom) /2
+        month = 0
+        balance = balancestat
+    if month == 12 and balance < 0.01:
+        bottom = midpoint
+        midpoint = (top - bottom) /2
+        month = 0
+        balance = balancestat
+    if balance <= -0.01 and balance >= 0.01:
+        print("Lowest Payment:", midpoint)
