8240be25e83c4b196b2e253303a2972e616ea5e3
williamstern <wmstern@stu.naperville203.org>
2017-02-02 03:03:45 -0600

    Add files via upload

---
+++ Set 2 Problem 3.py
@@ -0,0 +1,20 @@
+month = 0 
+balance = 4773
+bottom = balance * (1/12)
+annualInterestRate = 0.2
+balancestat = balance
+payment = 0
+monthlyinterestrate = annualInterestRate / 12
+while month < 12:
+   month += 1
+   unpaidbalance = balance - payment
+   interest = unpaidbalance * monthlyinterestrate
+   balance = interest + unpaidbalance
+   payment * month
+   if month == 12 and balance > 0:
+	   payment += 10
+	   month = 0
+	   balance = balancestat
+if balance <= 0:
+  print("Lowest Payment:", payment)
+  #print(balance)
