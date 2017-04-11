1fb1aef77fd9ae03c31105a566da05a72095562a
William Stern <wmstern@stu.naperville203.org>
2017-04-11 12:17:48 -0500

    Update vowel_count.py

--- vowel_count.py
+++ vowel_count.py
@@ -2,7 +2,7 @@ t_l = "alflkdsghsldghlksdfjaikldsg" #total letters
 counter = 0
 times_run = 0
 for letters in(t_l):
-		for letters in (str('a'), str('e'), str('i'), str('o'), str('u')):
+		for letters in ("aeiou"):
 			counter += 1
 	#counter += 1
 print('Number of vowels: ', counter)
